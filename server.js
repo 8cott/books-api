fc22c064d5493c45f0bda61097b6f77b6bbba52a
8cott <scottrubin@gmail.com>
2023-02-07 16:47:26 -0500

    Finished Project

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 // DEPENDENCIES
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 // CONFIG
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-)
+require('dotenv').config();
+const PORT = process.env.PORT;
+const app = express();
+app.use(cors());
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('connected to mongo: ', process.env.MONGO_URI);
+  }
+);
 
 // MIDDLEWARE
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Routes
+// INDEX
 app.get('/', (req, res) => {
-    res.send('Books API')
-})
+  res.send('Books API 📚');
+});
+
+// BOOKS
+const booksController = require('./controllers/books_controller.js');
+app.use('/books', booksController);
 
 // LISTEN
-app.listen(process.env.PORT) 
-    console.log('listening on port', PORT);
+app.listen(process.env.PORT);
+console.log('listening on port:', PORT);
